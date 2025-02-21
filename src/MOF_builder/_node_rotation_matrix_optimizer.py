fixed multitopic merged edges wrong order issue and flipped rot issue

src/MOF_builder/_node_rotation_matrix_optimizer.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize,differential_evolution
 from _place_node_edge import unit_cell_to_cartesian_matrix, fractional_to_cartesian
+from scipy.spatial.transform import Rotation as R
 
 def locate_min_idx(a_array):
     #print(a_array,np.min(a_array))
@@ -250,10 +251,62 @@ def compute_rotation_with_pairing(connected_nodes, atom_positions,current_rotati
     optimized_rotation_matrix = np.dot(R, current_rotation_matrix)
 
     return optimized_rotation_matrix
-
-def objective_function(params, G, static_atom_positions, sorted_nodes,sorted_edges):
+def objective_function_pre(params, G, static_atom_positions, sorted_nodes,sorted_edges):
     """
-    Objective function to minimize distances between paired atoms along edges.
+    Objective function to minimize distances between paired node to paired node_com along edges.
+
+    Parameters:
+        params (numpy.ndarray): Flattened array of rotation matrices.
+        G (networkx.Graph): Graph structure.
+        atom_positions (dict): Original positions of X atoms for each node.
+
+
+    Returns:
+        float: Total distance metric to minimize.
+    """
+    num_nodes = len(G.nodes())
+    rotation_matrices = params.reshape(num_nodes, 3, 3)
+    total_distance = 0.0
+
+    for (i, j) in sorted_edges:
+        R_i = reorthogonalize_matrix(rotation_matrices[i])
+
+        com_i = G.nodes[sorted_nodes[i]]['ccoords']
+        com_j = G.nodes[sorted_nodes[j]]['ccoords']
+        # Rotate positions around their mass center
+        rotated_i_positions = np.dot(static_atom_positions[i][:,1:] - com_i, R_i.T) + com_i
+
+
+        dist_matrix = np.empty((len(rotated_i_positions), 1))    
+        for idx_i in range(len(rotated_i_positions)):
+                dist = np.linalg.norm(rotated_i_positions[idx_i] - com_j)
+                dist_matrix[idx_i,0] = dist
+                #total_distance += dist ** 2
+        if np.argmin(dist_matrix) > 1:
+            total_distance += 1e4 #penalty for the distance difference
+        else:
+            total_distance += (np.min(dist_matrix) ** 2)
+#
+        for idx_i in range(len(rotated_i_positions)):
+            #second min and min distance difference not max
+            if len(dist_matrix[idx_i, :]) > 1:
+                second_min_dist = np.partition(dist_matrix[idx_i, :], 1)[1]
+            else:
+                second_min_dist = np.partition(dist_matrix[idx_i, :], 0)[0]
+            diff = second_min_dist - np.min(dist_matrix[idx_i, :])
+
+            if diff <4:
+                total_distance += 1e4
+        
+        total_distance+= 1e3/(np.max(dist_matrix) - np.min(dist_matrix) ) #reward for the distance difference
+
+   
+
+    return total_distance
+
+def objective_function_after(params, G, static_atom_positions, sorted_nodes,sorted_edges):
+    """
+    Objective function to minimize distances between paired atoms along edges. just use minimum distance
 
     Parameters:
         params (numpy.ndarray): Flattened array of rotation matrices.
@@ -284,28 +337,42 @@ def objective_function(params, G, static_atom_positions, sorted_nodes,sorted_edg
             for idx_j in range(len(rotated_j_positions)):
                 dist = np.linalg.norm(rotated_i_positions[idx_i] - rotated_j_positions[idx_j])
                 dist_matrix[idx_i, idx_j] = dist
-                total_distance += (dist ** 3)
-        for idx_i in range(len(rotated_i_positions)):
-            diff = max(dist_matrix[idx_i,:]) - min(dist_matrix[idx_i, :])
-            if diff > 1:
-                total_distance += 10000/diff 
-            else:#penalty for the distance difference
-                total_distance += 1e5
-        for idx_j in range(len(rotated_j_positions)):
-            diff = max(dist_matrix[:,idx_j]) - min(dist_matrix[:, idx_j])
-            if diff > 1:
-                total_distance += 10000/diff 
-            else:#penalty for the distance difference
-                total_distance += 1e5
+ 
+        if np.argmin(dist_matrix) > 1:
+            total_distance += 1e4 #penalty for the distance difference
+        else:
+            total_distance += (np.min(dist_matrix) ** 2)
 
+
+
+
+
+        for idx_i in range(len(rotated_i_positions)):
+            #second min and min distance difference not max
+            if len(dist_matrix[idx_i, :]) > 1:
+                second_min_dist = np.partition(dist_matrix[idx_i, :], 1)[1]
+            else:
+                second_min_dist = np.partition(dist_matrix[idx_i, :], 0)[0]
+            diff = second_min_dist - np.min(dist_matrix[idx_i, :])
+            if diff <3:
+                total_distance += 1e4
+        for idx_j in range(len(rotated_j_positions)):
+            #second min and min distance difference not max
+            if len(dist_matrix[:, idx_j]) > 1:
+                second_min_dist = np.partition(dist_matrix[:, idx_j], 1)[1]
+            else:
+                second_min_dist = np.partition(dist_matrix[:, idx_j], 0)[0]
+            diff = second_min_dist - np.min(dist_matrix[:, idx_j])
+
+            if diff <3:
+                total_distance += 1e4
+        
 
     return total_distance
 
 
-
-
-
-def optimize_rotations(num_nodes,G,sorted_nodes,sorted_edges,atom_positions,opt_methods="L-BFGS-B",maxfun=15000,maxiter=5000):
+def optimize_rotations_pre(num_nodes, G, sorted_nodes, sorted_edges, atom_positions, initial_rotations, opt_method,
+                               maxfun, maxiter, disp, eps, iprint):
     """
     Optimize rotations for all nodes in the graph.
 
@@ -316,7 +383,8 @@ def optimize_rotations(num_nodes,G,sorted_nodes,sorted_edges,atom_positions,opt_
     Returns:
         list: Optimized rotation matrices for all nodes.
     """
-    initial_rotations = np.tile(np.eye(3), (num_nodes, 1)).flatten()
+    print('optimize_rotations_step1')
+    #initial_rotations = np.tile(np.eye(3), (num_nodes, 1)).flatten()
     #get a better initial guess, use random rotation matrix combination
     #initial_rotations  = np.array([reorthogonalize_matrix(np.random.rand(3,3)) for i in range(num_nodes)]).flatten()
     static_atom_positions = atom_positions.copy()
@@ -324,11 +392,64 @@ def optimize_rotations(num_nodes,G,sorted_nodes,sorted_edges,atom_positions,opt_
     #edge_pairings = find_edge_pairings(sorted_edges, atom_positions)
 
     result = minimize(
-        objective_function,
+        objective_function_pre,
+        initial_rotations,
+        args=(G, static_atom_positions, sorted_nodes, sorted_edges),
+        method=opt_method,
+        options={'maxfun': maxfun, 
+                 'maxiter': maxiter, 
+                 'disp': disp, 
+                 'eps': eps, 
+                 'iprint': iprint, },
+    )
+
+    
+
+    #optimized_rotations = result.x.reshape(num_nodes, 3, 3)
+    #optimized_rotations = [reorthogonalize_matrix(R) for R in optimized_rotations]
+    
+    optimized_rotations = result.x
+    #optimized_rotations = [reorthogonalize_matrix(R) for R in optimized_rotations]
+   ## # Print the optimized pairings after optimization
+   ## print("Optimized Pairings (after optimization):")
+   ## for (i, j), pairs in edge_pairings.items():
+   ##     print(f"Node {i} and Node {j}:")
+   ##     for idx_i, idx_j in pairs:
+   ##         print(f"  node{i}_{idx_i} -- node{j}_{idx_j}")
+   ## print()
+
+    return optimized_rotations,static_atom_positions
+
+
+def optimize_rotations_after(num_nodes, G, sorted_nodes, sorted_edges, atom_positions, initial_rotations, opt_method,
+                               maxfun, maxiter, disp, eps, iprint,):
+    """
+    Optimize rotations for all nodes in the graph.
+
+    Parameters:
+        G (networkx.Graph): Graph structure with edges between nodes.
+        atom_positions (dict): Positions of X atoms for each node.
+
+    Returns:
+        list: Optimized rotation matrices for all nodes.
+    """
+    print('optimize_rotations_step2')
+    #get a better initial guess, use random rotation matrix combination
+    #initial_rotations  = np.array([reorthogonalize_matrix(np.random.rand(3,3)) for i in range(num_nodes)]).flatten()
+    static_atom_positions = atom_positions.copy()
+    # Precompute edge-specific pairings
+    #edge_pairings = find_edge_pairings(sorted_edges, atom_positions)
+
+    result = minimize(
+        objective_function_after,
         initial_rotations,
         args=(G, static_atom_positions, sorted_nodes,sorted_edges),
-        method=opt_methods,
-        options={"maxiter": maxiter, "disp": True,"maxfun": maxfun},
+        method= opt_method,
+        options={'maxfun': maxfun, 
+            'maxiter': maxiter, 
+            'disp': disp, 
+            'eps': eps, 
+            'iprint': iprint, },
     )
 
     
@@ -345,6 +466,10 @@ def optimize_rotations(num_nodes,G,sorted_nodes,sorted_edges,atom_positions,opt_
    ## print()
 
     return optimized_rotations,static_atom_positions
+
+
+
+
 
 def apply_rotations_to_atom_positions(optimized_rotations, G,sorted_nodes, atom_positions):
     """
@@ -460,7 +585,7 @@ def find_edge_pairings(sorted_nodes,sorted_edges, atom_positions):
 
     return edge_pairings
 
-def apply_rotations_to_xxxx_positions(optimized_rotations, G,sorted_nodes,sorted_edges_of_sortednodeidx, xxxx_positions_dict):
+def apply_rotations_to_Xatoms_positions(optimized_rotations, G,sorted_nodes,sorted_edges_of_sortednodeidx, Xatoms_positions_dict):
     """
     Apply the optimized rotation matrices to the atom positions.
 
@@ -472,7 +597,7 @@ def apply_rotations_to_xxxx_positions(optimized_rotations, G,sorted_nodes,sorted
     Returns:
         dict: Rotated positions for each node.
     """
-    rotated_positions = xxxx_positions_dict.copy()
+    rotated_positions = Xatoms_positions_dict.copy()
 
     for i, node in enumerate(sorted_nodes):
         #if node type is V

src/MOF_builder/_terminations.py
@@ -54,12 +54,16 @@ def Xpdb(data,X):
 def convert_to_tuple(array):
     return (tuple(np.round(array[0],3).flatten()), tuple(np.round(array[1],3).flatten()),tuple(np.round(array[2],3).flatten()))
 
+##def is_list_A_in_B(A, B):
+##    # Convert both A and B into sets of tuple representations
+##    A_tuples = set(convert_to_tuple(a) for a in A)
+##    B_tuples = set(convert_to_tuple(b) for b in B)
+##    # Check if all elements in A are in B
+##    return A_tuples.issubset(B_tuples)
+
 def is_list_A_in_B(A, B):
-    # Convert both A and B into sets of tuple representations
-    A_tuples = set(convert_to_tuple(a) for a in A)
-    B_tuples = set(convert_to_tuple(b) for b in B)
-    # Check if all elements in A are in B
-    return A_tuples.issubset(B_tuples)
+    return all([np.allclose(a, b, atol=1e-3) for a, b in zip(A, B)])
+
 
 def add_terminations(term_file,ex_node_cxo_cc):
     tG=nx.Graph()

src/MOF_builder/multiedge_bundling.py
@@ -1,30 +1,46 @@
 import numpy as np
 from scipy.optimize import linear_sum_assignment
+from v2_functions import fractional_to_cartesian
 
-def find_pair_x_edge(x_matrix,edge_matrix):
+
+def find_pair_x_edge(x_matrix, edge_matrix):
     dist_matrix = np.zeros((len(x_matrix), len(edge_matrix)))
     for i in range(len(x_matrix)):
         for j in range(len(edge_matrix)):
-            dist_matrix[i,j] = np.linalg.norm(x_matrix[i]-edge_matrix[j])
+            dist_matrix[i, j] = np.linalg.norm(x_matrix[i] - edge_matrix[j])
     row_ind, col_ind = linear_sum_assignment(dist_matrix)
-    return row_ind,col_ind
+    return row_ind, col_ind
+
+
+def find_pair_x_edge_fc(x_matrix, edge_matrix, sc_unit_cell):
+    dist_matrix = np.zeros((len(x_matrix), len(edge_matrix)))
+    x_matrix = fractional_to_cartesian(x_matrix, sc_unit_cell)
+    edge_matrix = fractional_to_cartesian(edge_matrix, sc_unit_cell)
+    for i in range(len(x_matrix)):
+        for j in range(len(edge_matrix)):
+            dist_matrix[i, j] = np.linalg.norm(x_matrix[i] - edge_matrix[j])
+    row_ind, col_ind = linear_sum_assignment(dist_matrix)
+    ##print(row_ind, col_ind) # debug
+    return row_ind, col_ind
+
 
 def bundle_multiedge(sG):
     multiedge_bundlings = []
     for n in sG.nodes:
-        if 'CV' in n and sG.nodes[n]['type']=='V':
-            edges =[]
+        if "CV" in n and sG.nodes[n]["type"] == "V":
+            edges = []
             for con_n in list(sG.neighbors(n)):
-                edges.append(sG.edges[n,con_n]['coords'])
-            edges = np.vstack(edges,dtype=float)
-            #extract the X atom in the CV node
-            cv_xatoms = np.asarray(sG.nodes[n]['x_coords'][:,2:5],dtype=float) #modified for extra column of atom type
-            if len(cv_xatoms) < len(edges):
-                #duplicate the cv_xatoms
-                cv_xatoms = np.vstack([cv_xatoms]*len(edges))
-            _,order = find_pair_x_edge(cv_xatoms,edges)
-            con_n_order = [list(sG.neighbors(n))[i] for i in order]
-            multiedge_bundlings.append((n,con_n_order))
-            #make dist matrix of each x to each edge and then use hugerian algorithm to find the pair of x and edge
+                edges.append(sG.edges[n, con_n]["coords"])
+                # edges = np.vstack(edges, dtype=float)
+                ## extract the X atom in the CV node
+                # cv_xatoms = np.asarray(
+                #    sG.nodes[n]["x_coords"][:, 2:5], dtype=float
+                # )  # modified for extra column of atom type
+                # if len(cv_xatoms) < len(edges):
+                #    # duplicate the cv_xatoms
+                #    cv_xatoms = np.vstack([cv_xatoms] * len(edges))
+                ##_, order = find_pair_x_edge(cv_xatoms, edges)
+                ##con_n_order = [list(sG.neighbors(n))[i] for i in order]
+                multiedge_bundlings.append((n, list(sG.neighbors(n))))
+            # make dist matrix of each x to each edge and then use hugerian algorithm to find the pair of x and edge
     return multiedge_bundlings
-